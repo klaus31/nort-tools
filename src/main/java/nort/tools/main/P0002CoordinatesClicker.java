fdf1ec284726499981f0fdd0bd50b9df648e19f2
klaus31 <danieloltmanns@gmx.de>
2015-11-12 21:32:57 +0100

    coordinates clicker

--- src/main/java/nort/tools/main/P0002CoordinatesClicker.java
+++ src/main/java/nort/tools/main/P0002CoordinatesClicker.java
@@ -1,5 +1,6 @@
 package nort.tools.main;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -20,20 +21,53 @@ import org.json.JSONObject;
 
 public class P0002CoordinatesClicker extends CoordinatesClicker2 {
 
-  private String outputFile;
-  private JSONArray outputJson;
-  private JSONArray points = new JSONArray();
-
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     // launch it
     Application.launch(args);
   }
 
+  private String outputFile;
+  private JSONArray outputJson;
+  private JSONArray points = new JSONArray();
+
+  private String getWebColor(final int x, final int y) {
+    Color color = image.getPixelReader().getColor(x, y);
+    return String.format("#%02X%02X%02X", (int) (color.getRed() * 255), (int) (color.getGreen() * 255), (int) (color.getBlue() * 255));
+  }
+
   @Override
-  public void start(Stage stage) throws IOException {
+  protected void handleMouseEvent(final MouseEvent event, final int x, final int y) {
+    if (event.getButton().equals(MouseButton.SECONDARY)) {
+      // add color and start new area
+      JSONObject area = new JSONObject();
+      String color = getWebColor(x, y);
+      System.out.println("Add color: " + color + " and finalize area");
+      area.put("color", color);
+      area.put("points", points);
+      outputJson.put(area);
+      points = new JSONArray();
+    } else {
+      // add point
+      System.out.println(String.format("x:%s;y:%s", x, y));
+      points.put(new JSONObject("{\"x\":" + x + ",\"y\":" + y + "}"));
+    }
+  }
+
+  @Override
+  public void setImage() {
+    image = new Image(NortConfig.get("p0002.imageFile"), 1000, 750, true, true);
+  }
+
+  @Override
+  public void start(final Stage stage) throws IOException {
     super.start(stage);
     stage.setFullScreen(true);
     outputFile = NortConfig.get("p0002.outputFile");
+    File oFCheck = new File(outputFile);
+    if (!oFCheck.canRead() || !oFCheck.canWrite()) {
+      System.out.println("echo [] > " + oFCheck.getAbsolutePath() + " # first");
+      System.exit(2);
+    }
     // set json
     String jsonText = "[]";
     try {
@@ -52,34 +86,6 @@ public class P0002CoordinatesClicker extends CoordinatesClicker2 {
       writer.close();
     } catch (FileNotFoundException | UnsupportedEncodingException e) {
       e.printStackTrace();
-    }
-  }
-
-  private String getWebColor(int x, int y) {
-    Color color = image.getPixelReader().getColor(x, y);
-    return String.format("#%02X%02X%02X", (int) (color.getRed() * 255), (int) (color.getGreen() * 255), (int) (color.getBlue() * 255));
-  }
-
-  @Override
-  public void setImage() {
-    image = new Image(NortConfig.get("p0002.imageFile"), 1000, 750, true, true);
-  }
-
-  @Override
-  protected void handleMouseEvent(MouseEvent event, int x, int y) {
-    if (event.getButton().equals(MouseButton.SECONDARY)) {
-      // add color and start new area
-      JSONObject area = new JSONObject();
-      String color = getWebColor(x, y);
-      System.out.println("Add color: " + color + " and finalize area");
-      area.put("color", color);
-      area.put("points", points);
-      outputJson.put(area);
-      points = new JSONArray();
-    } else {
-      // add point
-      System.out.println(String.format("x:%s;y:%s", x, y));
-      points.put(new JSONObject("{\"x\":" + x + ",\"y\":" + y + "}"));
     }
   }
 }
